8ef2d1dc867ddcf02debcc1f0cda90d2652a1ea5
wangfeng010 <1977513290@qq.com>
2025-05-15 10:01:06 +0800

    userPropernoun缺失的时填充默认值

--- data/train.sql
+++ data/train.sql
@@ -1,134 +1,17 @@
-select
-	sample.user_id as user_id,
-	sample.create_time as create_time,
-	sample.label as log_type,
-	kyc.watchlists as watchlists,
-	kyc.holdings as holdings,
-	kyc.country as country,
-	kyc.prefer_bid as prefer_bid,
-	kyc.user_propernoun as user_propernoun,
-	sample.push_title as push_title,
-	sample.push_content as push_content,
-	sample.item_code as item_code,
-	sample.tags as item_tags,
-	sample.submit_type as submit_type
-from
-	(
-		select
-			Behavior.user_id as user_id,
-			Behavior.create_time as create_time,
-			Behavior.log_type as label,
-			Item1.item_id as item_id,
-			Item1.push_title as push_title,
-			Item1.push_content as push_content,
-			Item2.item_code as item_code,
-			Item2.tags as tags,
-			Item2.submitType as submit_type
-		from
-			(
-				select
-					user_id,
-					push_id,
-					create_time,
-					log_type
-				from
-					hx_dwd.dwd_mkt_push_detail_ds
-				where
-					p_date = '{now}'
-					and log_type in ('PC', 'PR')
-					and user_id > 0
-			) Behavior
-			inner join (
-				select
-					push_id,
-					item_id,
-					push_title,
-					push_content
-				from
-					hx_dim.dim_mkt_push_base_info_ds
-				where
-					p_date = '{now}'
-					and come_from in (
-						'999677',
-						'999678',
-						'999679',
-						'999680',
-						'999681',
-						'999682',
-						'999684'
-					)
-			) Item1 on Behavior.push_id = Item1.push_id
-			left join (
-				select
-					item_id,
-					logmap ['stocks'] as item_code,
-					logmap ['tags'] as tags,
-					logmap ['submitType'] as submitType
-				from
-					hx_dim.dim_mkt_rec_res_pool_item_hd
-				where
-					p_date = '{now}'
-			) Item2 on Item1.item_id = Item2.item_id
-		where
-			if(
-				Behavior.log_type = 'PC',
-				1,
-				rand(10)
-			) > 0.9
-	) sample
-	left join (
-		select
-			person_id,
-			max(
-				case
-					when l_code = 'BM79'
-					and l_value is not null then l_value
-					else null
-				end
-			) as active_time,
-			max(
-				case
-					when l_code = 'BM176'
-					and l_value is not null then l_value
-					else null
-				end
-			) as watchlists,
-			max(
-				case
-					when l_code = 'BM210'
-					and l_value is not null then l_value
-					else null
-				end
-			) as holdings,
-			max(
-				case
-					when l_code = 'BM55'
-					and l_value is not null then l_value
-					else null
-				end
-			) as country,
-			max(
-				case
-					when l_code = 'BM70'
-					and l_value is not null then l_value
-					else null
-				end
-			) as prefer_bid,
-			max(
-				case
-					when l_code = 'BM230'
-					and l_value is not null then l_value
-					else null
-				end
-			) as user_propernoun
-		from
-			db_dws.dws_crd_lb_v_dd
-		where
-			p_date = '{now}'
-			and l_code in ('BM79', 'BM176', 'BM210', 'BM55', 'BM70', 'BM230')
-			and person_id > 0
-		group by
-			person_id
-	) kyc on sample.user_id = kyc.person_id
-where
-	sample.user_id is not null;
+-- 从优化后的临时表中获取结果数据
+SELECT 
+    user_id,
+    create_time,
+    log_type,
+    watchlists,
+    holdings,
+    country,
+    prefer_bid,
+    user_propernoun,
+    push_title,
+    push_content,
+    item_code,
+    item_tags,
+    submit_type
+FROM 
+    db_test.tmp_final_result_{now}
